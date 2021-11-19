72adc285a552e7ea7e6c8dd34f40aa264de4ec63
ariaalinejad <79251626+ariaalinejad@users.noreply.github.com>
2021-11-19 09:41:44 +0100

    Update pixelTop.v

--- Verilog/pixelTop.v
+++ Verilog/pixelTop.v
@@ -8,13 +8,14 @@ module PIXEL_TOP (CLOCK, RESET, ERASE, EXPOSE, CONVERT, READ, VBN1, ANARESET, RA
     input logic               RAMP;
     inout logic             EXPOSE;
     inout logic            CONVERT;
-    inout logic [row-1:0]         READ;
+    inout logic [row-1:0]         READ; // each row needs its own read
     inout logic              ERASE;
-    inout [row-1:0][7:0]          DATA;
+    inout [col-1:0][7:0]          DATA; / we need one 8-bit bus for each column
 
-    parameter int col=4,row=4;
-    parameter [row*col*4-1:0] dv_pixel = 0;
+    parameter int col=4,row=4; // this will be overwritten in pixelTop_tb
+    parameter [row*col*4-1:0] dv_pixel = 0; // this will be overwritten in pixelTop_tb
 
+    // We instansiate a PIXEL_ARRAY and a PIXEL_STATE module so that PIXEL_STATE can contsol the states of each pixel in PIXEL_ARRAY
     PIXEL_ARRAY #(.dv_pixel(dv_pixel), .col(col), .row(row)) pa1 (VBN1, RAMP, ANARESET, ERASE, EXPOSE, READ, DATA);
     PIXEL_STATE #(.col(col), .row(row)) ps1 (CLOCK, RESET, ERASE, EXPOSE, CONVERT, READ);
 
