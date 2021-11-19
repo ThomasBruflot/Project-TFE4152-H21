667b172078fbd5d4c209dac1df91b5cafb2d6867
ariaalinejad <79251626+ariaalinejad@users.noreply.github.com>
2021-11-19 09:41:22 +0100

    Update pixelState_tb.v

--- Verilog/pixelState_tb.v
+++ Verilog/pixelState_tb.v
@@ -8,19 +8,20 @@ module PIXEL_STATE_TB ();
 	logic convert;
 	logic [row-1:0] read;
 
-    parameter int col=4,row=4;
+    parameter int col=4,row=4; // we test for a 4x4 system
+    parameter real dv_pixel = 5;  //Set the expected photodiode current (0-1)
 
-    initial begin
+    initial begin // we make a clock 
         clock = 0;
         forever #10 clock = ~clock;
     end  
 
-    PIXEL_STATE dout(clock, reset, erase, expose, convert, read);
+    PIXEL_STATE #(.dv_pixel(dv_pixel)) dout(clock, reset, erase, expose, convert, read);
 
     initial begin
-        $dumpfile("pixelState_tb.vcd");
+        $dumpfile("pixelState_tb.vcd"); // we dump the result into a vcd file that GTKwave can read
         $dumpvars();
-		reset = 1; #20;
+		reset = 1; #20; // we reset, wait a while and then reset again 
 		reset = 0; #15000;
         reset = 1; #20;
         reset = 0; #1000;
