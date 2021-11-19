538de13d68390c89d71d996ecf862851586567bc
ariaalinejad <79251626+ariaalinejad@users.noreply.github.com>
2021-11-19 09:38:18 +0100

    Update pixelArray_tb.v

--- Verilog/pixelArray_tb.v
+++ Verilog/pixelArray_tb.v
@@ -1,15 +1,6 @@
 `timescale 1 ns / 1 ps
 
-//====================================================================
-// Testbench for pixelArray
-// - clock
-// - instanciate pixel
-// - State Machine for controlling each pixel sensor
-// - Model the DAC and ADC
-// - Readout of the databus
-// - Stuff neded for testbench. Store the output file etc.
-//====================================================================
-module pixelArray_tb;
+module pixelTop_tb;
 
    //------------------------------------------------------------
    // Testbench clock
@@ -19,129 +10,55 @@ module pixelArray_tb;
    parameter integer clk_period = 500;
    parameter integer sim_end = clk_period*2400;
    parameter integer test_reset = clk_period*3700; // 3600 + 100
-   always #clk_period clk=~clk;
+   always #clk_period clk=~clk; // we make the clock 
 
    //------------------------------------------------------------
    // Pixel
    //------------------------------------------------------------
-   parameter real    dv_pixel = 0.5;  //Set the expected photodiode current (0-1)
+   /*
+   parameter [row*col*4-1:0] dv_pixel = {4'd5, 4'd5, 4'd5, 4'd5, 4'd5, 4'd5, 4'd4, 4'd3, 4'd2, 4'd1,
+                                         4'd5, 4'd5, 4'd5, 4'd5, 4'd5, 4'd5, 4'd5, 4'd4, 4'd3, 4'd2,
+                                         4'd5, 4'd5, 4'd5, 4'd5, 4'd5, 4'd5, 4'd6, 4'd5, 4'd4, 4'd3,
+                                         4'd5, 4'd5, 4'd5, 4'd5, 4'd5, 4'd5, 4'd7, 4'd6, 4'd5, 4'd4,
+                                         4'd5, 4'd5, 4'd5, 4'd5, 4'd5, 4'd5, 4'd8, 4'd7, 4'd6, 4'd5,
+                                         4'd5, 4'd5, 4'd5, 4'd5, 4'd5, 4'd5, 4'd9, 4'd8, 4'd7, 4'd6,
+                                         4'd5, 4'd5, 4'd5, 4'd5, 4'd5, 4'd5, 4'd1, 4'd9, 4'd8, 4'd7,
+                                         4'd5, 4'd5, 4'd5, 4'd5, 4'd5, 4'd5, 4'd2, 4'd1, 4'd9, 4'd8,
+                                         4'd5, 4'd5, 4'd5, 4'd5, 4'd5, 4'd5, 4'd3, 4'd2, 4'd1, 4'd9,
+                                         4'd5, 4'd5, 4'd5, 4'd5, 4'd5, 4'd5, 4'd4, 4'd3, 4'd2, 4'd1};
+   */
+   parameter [row*col*4-1:0] dv_pixel = {4'd1,4'd2, 4'd3,4'd4,   // we give the pixels different voltage values
+                                         4'd5,4'd6, 4'd7,4'd8,
+                                         4'd9,4'd10,4'd1,4'd2,   
+                                         4'd3,4'd4, 4'd5,4'd6};
+   parameter int col=4,row=4; // here we set the number of pixels per row and per column for the whole system
 
    //Analog signals
-   logic              anaBias1;
-   logic              anaRamp;
-   logic              anaReset;
+   logic         anaBias1;
+   logic          anaRamp;
+   logic         anaReset;
 
    //Tie off the unused lines
-   assign anaReset = 1;
+   assign anaReset = 1; // is always high
 
    //Digital
-   logic              erase;
-   logic              expose;
-   logic [3:0]          read;
-   tri[3:0][7:0]     pixData; //  We need this to be a wire, because we're tristating it
+   logic                       erase;
+   logic                      expose;
+   logic [row-1:0]              read; // one read per row
+   tri[col-1:0][7:0]         pixData; // one data variable per column
+   logic                     convert;
 
-   //Instanciate the pixels
-   PIXEL_ARRAY  pa1(anaBias1, anaRamp, anaReset, erase,expose, read,pixData);
+   // We split up the output data for easier reading 
+   logic [7:0] pixelDataOut_0,  pixelDataOut_1, pixelDataOut_2, pixelDataOut_3;
+   
 
-   //------------------------------------------------------------
-   // State Machine
-   //------------------------------------------------------------
-   parameter ERASE=0, EXPOSE=1, CONVERT=2, READ=3, IDLE=4;
-
-   logic               convert;
-   logic [2:0]    state,next_state;   //States
-   integer             counter;       //Delay counter in state machine
-
-   //State duration in clock cycles
-   parameter integer c_erase = 5;
-   parameter integer c_expose = 255;
-   parameter integer c_convert = 255;
-   parameter integer c_read = 5;
-
-   // Control the output signals
-   always_ff @(negedge clk ) begin
-      case(state)
-        ERASE: begin
-           erase <= 1;
-           read <= 4'b0000;
-           expose <= 0;
-           convert <= 0;
-        end
-        EXPOSE: begin
-           erase <= 0;
-           read <= 4'b0000;
-           expose <= 1;
-           convert <= 0;
-        end
-        CONVERT: begin
-           erase <= 0;
-           read <= 4'b0000;
-           expose <= 0;
-           convert = 1;
-        end
-        READ: begin
-           erase <= 0;
-           read <= 4'b0001;
-           expose <= 0;
-           convert <= 0;
-        end
-        IDLE: begin
-           erase <= 0;
-           read <= 4'b0000;
-           expose <= 0;
-           convert <= 0;
-
-        end
-      endcase // case (state)
-   end // always @ (state)
-
-   // Control the state transitions
-   always_ff @(posedge clk or posedge reset) begin
-      if(reset) begin
-         state = IDLE;
-         next_state = ERASE;
-         counter  = 0;
-         convert  = 0;
-      end
-      else begin
-         case (state)
-           ERASE: begin
-              if(counter == c_erase) begin
-                 next_state <= EXPOSE;
-                 state <= IDLE;
-              end
-           end
-           EXPOSE: begin
-              if(counter == c_expose) begin
-                 next_state <= CONVERT;
-                 state <= IDLE;
-              end
-           end
-           CONVERT: begin
-              if(counter == c_convert) begin
-                 next_state <= READ;
-                 state <= IDLE;
-              end
-           end
-           READ:
-             if(counter == c_read) begin
-                state <= IDLE;
-                next_state <= ERASE;
-             end
-           IDLE:
-             state <= next_state;
-         endcase // case (state)
-         if(state == IDLE)
-           counter = 0;
-         else
-           counter = counter + 1;
-      end
-   end // always @ (posedge clk or posedge reset)
+   //Instanciate the pixel and set dv_pixel and number of pixels for the whole system 
+   PIXEL_TOP #(.dv_pixel(dv_pixel), .col(col), .row(row)) pt1(clk, reset, erase, expose, convert, read, anaBias1, anaReset, anaRamp, pixData);
 
    //------------------------------------------------------------
    // DAC and ADC model
    //------------------------------------------------------------
-   logic[3:0][7:0] data;
+   logic[col-1:0][7:0] data; // one data bus per column 
 
 
    // If we are to convert, then provide a clock via anaRamp
@@ -154,67 +71,70 @@ module pixelArray_tb;
    assign anaBias1 = expose ? clk : 0;
    
 
-   // If we're not reading the pixData, then we should drive the bus
-   assign pixData[0] = read? 8'bZ: data[0];
-   assign pixData[1] = read? 8'bZ: data[1];
-   assign pixData[2] = read? 8'bZ: data[2];
-   assign pixData[3] = read? 8'bZ: data[3];
+   // each pixData is set to be data for each pixel in the row, while not reading. While reading it is set to a high impedance state
+   generate
+      genvar i;
+      for (i = 0; i<col; i = i + 1)begin
+         assign pixData[i] = read? 8'bZ: data[i]; 
+      end
+   endgenerate
+
+   // pixelDataOut is plit into 4 variables for easier reading 
+   assign pixelDataOut_0 = pixelDataOut[0];
+   assign pixelDataOut_1 = pixelDataOut[1];
+   assign pixelDataOut_2 = pixelDataOut[2];
+   assign pixelDataOut_3 = pixelDataOut[3];
 
    // When convert, then run a analog ramp (via anaRamp clock) and digtal ramp via
    // data bus. Assert convert_stop to return control to main state machine.
    always_ff @(posedge clk or posedge reset) begin
       if(reset) begin
-          data = 4'b0000;
+         data = 0;
       end
       if(convert) begin
-         data[0] +=  1;
-         data[1] +=  1;
-         data[2] +=  1;
-         data[3] +=  1;
+         for (int i=0;i<col;i=i+1) begin
+            data[i] += 1; // data counts upward while converting, pixData is then set to this value so that each pixel takes in a digital ramp 
+         end
       end
       else begin
-         data = 4'b0000;
+         data = 0;
       end
    end // always @ (posedge clk or reset)
-
 
    //------------------------------------------------------------
    // Readout from databus
    //------------------------------------------------------------
-   logic [3:0][7:0] pixelDataOut;
+   logic [col-1:0][7:0] pixelDataOut; // one for each column 
    always_ff @(posedge clk or posedge reset) begin
       if(reset) begin
-         pixelDataOut = 4'b0000;
+         pixelDataOut = 0;
       end
       else begin
          if(read) begin
-            pixelDataOut[0] <= pixData[0];
-            pixelDataOut[1] <= pixData[1];
-            pixelDataOut[2] <= pixData[2];
-            pixelDataOut[3] <= pixData[3];
+            for (int i=0;i<col;i=i+1)begin
+               pixelDataOut[i] <= pixData[i]; // After convet is done each pixel on the row has latched a value that they output through pixData, pixelDataOut is here set to this value  
+            end
          end
          else begin
-            pixelDataOut = 4'b0000;
+            pixelDataOut = 0;
          end
       end
    end
    
-
    //------------------------------------------------------------
    // Testbench stuff
    //------------------------------------------------------------
-   initial
-     begin
-        $dumpfile("pixelArray_tb.vcd");
-        $dumpvars(0,pixelArray_tb);
+   initial begin
+      $dumpfile("pixelTop_tb.vcd"); // we dump the result into a vcd file that GTKwave can read
+      $dumpvars(0,pixelTop_tb);
+      
+      reset=1; #clk_period; // we reset, wait a while and reset again 
+      reset=0; #test_reset;  // test_reset = clk_period*3700
+      reset=1; #clk_period;  
+      reset=0;
 
-        reset=1; #clk_period;
-        reset=0; #test_reset;  
-        reset=1; #clk_period;  
-        reset=0;
-
-        #sim_end
-          $stop;
-     end
+      #sim_end
+         $stop;
+   end
 
 endmodule // test
